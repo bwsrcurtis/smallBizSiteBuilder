0ed4456709422677db14d0e98465bbd23875ceb7
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-02 19:29:10 -0400

    Improved responsiveness of hero

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -7,8 +7,8 @@ const Hero = () => {
 			<div className='-z-10'>
 				<Image src="/Hero.jpg" fill={true} alt="Beach Image" className="filter saturate-[.25]"></Image>
 			</div>
-			<div className='relative flex flex-col md:items-center md:justify-center h-5/6 space-y-6'>
-				<h1 className='text-2xl md:text-8xl text-primary'>Beachside Design</h1>
+			<div className='relative flex flex-col items-center justify-center h-5/6 space-y-6'>
+				<h1 className='text-4xl md:text-7xl lg:text-8xl text-primary'>Beachside Design</h1>
 				<p className='text-lg md:text-4xl text-white'>We Help Small Businesses Get Online</p>
 			</div>
 		</div>
