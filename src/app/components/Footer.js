77c92919cf352fff7c704916821e195be95aae2d
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-02 17:24:46 -0400

    ReadMe and various updates

---
+++ src/app/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<footer className='flex justify-between'>
+			<h1 className='text-white'>Header</h1>
+			<h1 className='text-white'>Header</h1>
+
+		</footer>
+	)
+}
+
+export default Footer
