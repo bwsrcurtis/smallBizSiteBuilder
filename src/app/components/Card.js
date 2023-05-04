6007ab23e5d36da2584e009d76f2be0902b21429
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-04 17:37:50 -0400

    Card formatting

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -4,12 +4,12 @@ import Image from 'next/image';
 const Card = ({ src, alt, title, description }) => {
 
 	return (
-		<div className='relative w-full xl:w-[20rem]'>
-			<div className='bg-primary/75 mx-auto invisible lg:visible xl:w-[20rem] h-96 w-[calc(full-2rem)] absolute top-[-.5rem] left-[-1rem] rounded-lg'></div>
-			<div className='h-96 mx-auto xl:w-[20rem] relative bg-secondary rounded-lg'>
+		<div className='relative w-[60vw] md:w-[50vw] lg:w-[20rem] h-fit'>
+			<div className='bg-primary/75 mx-4 invisible lg:visible lg:w-[20rem] h-full w-[calc(full-2rem)] absolute top-[-.5rem] left-[-1rem] rounded-lg'></div>
+			<div className='h-full mx-4 xl:w-[20rem] relative bg-secondary rounded-lg'>
 				<Image src={src} alt={alt} width={320} height={192} className="h-48 w-full rounded-t-lg"></Image>
-				<p className='text-left pl-2 pt-2 text-2xl text-primary mb-6'>{title}</p>
-				<p className='pl-2 text-text'>{description}</p>
+				<p className='text-left pl-2 pt-2 text-2xl w-full text-primary mb-6'>{title}</p>
+				<p className='pl-2 text-text pb-6 w-full'>{description}</p>
 			</div>
 		</div>
 	);
