cda41255522211f66b4196ec55a31f39fa2654e8
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-07 19:42:13 -0400

    adjusted line height

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import GetStarted from './components/GetStarted';
 
 export default function Home() {
   return (
-    <main className="bg-secondary min-h-[calc(100vh-112px)]">
+    <main className="bg-secondary min-h-[calc(100vh-112px)] leading-relaxed">
       <Hero></Hero>
       <Offerings></Offerings>
       <Examples></Examples>
