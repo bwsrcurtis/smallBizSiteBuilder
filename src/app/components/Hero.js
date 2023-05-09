738e09b14cdddff43d64c9591ba40364352093e7
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-09 14:49:01 -0400

    Hero responsiveness

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -4,24 +4,25 @@ import Heroimage from '../../../public/Heroimage.jpg';
 
 const Hero = () => {
 	return (
-		<div className="relative w-screen h-[860px]">
+		<div className="relative w-screen h-[600px] md:h-[860px]">
 			<div className='-z-10 static'>
 				<Image
 					priority src={Heroimage} fill={true} alt="Beach Image"
-					className="filter saturate-[.35] xl:object-fill object-cover">
+					className="filter saturate-[.35] md:object-fill object-cover">
 				</Image>
 			</div>
-			<div className='relative flex flex-col items-center justify-center w-full h-[860px] mx-auto'>
+			<div className='relative flex flex-col items-center
+			justify-center w-full h-[600px] md:h-[860px] mx-auto'>
 				<h1 className='text-5xl text-center font-bold sm:text-7xl lg:text-8xl text-primary
-				translate-y-[-103px]
-				sm:translate-y-[-112px]
+				translate-y-[-73px]
+				sm:translate-y-[-82px]
 				md:translate-y-[-106px]
 				lg:translate-y-[-112px]'>
 					Beachside Design
 				</h1>
 				<p className='text-xl text-center md:text-3xl lg:text-4xl text-white
-				translate-y-[-103px]
-				sm:translate-y-[-112px]
+				translate-y-[-73px]
+				sm:translate-y-[-82px]
 				md:translate-y-[-106px]
 				lg:translate-y-[-112px]'>
 					We Help Local Businesses Get Online
